98d445ffa05615be75f29caf9125296dbb7c6203
Akhila707 <akhilaprojects1@gmail.com>
2025-07-12 13:21:13 +0530

    chore: removed backend files from main branch

--- Routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signup, getUsers, createSwap } from '../controllers.js';
-const router = express.Router();
-
-router.post('/auth/signup', signup);
-router.get('/users', getUsers);
-router.post('/requests', createSwap);
-
-export default router;
